721f61a439035715cfa7ece9a5b0150a272fc859
M. Feustel <mike.feustel@it2011.ba-leipzig.de>
2014-01-09 17:29:25 +0100

    autocommit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,18 @@
 class UserMailer < ActionMailer::Base
+  include Devise::Mailers::Helpers
+
   default from: "info@BiCoR.com"
 
+  def confirmation_instructions(record)
+    devise_mail(record, :confirmation_instructions)
+  end
+
+  def reset_password_instructions(record)
+    devise_mail(record, :reset_password_instructions)
+  end
+
+  def unlock_instructions(record)
+    devise_mail(record, :unlock_instructions)
+  end
 
 end
