2354d4327dd0ec3bedf8a6f453de565833381d7f
mfeustel <mike.feustel@it2011.ba-leipzig.de>
2014-02-28 22:20:06 +0100

    autocommit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@
 class SessionsController < Devise::SessionsController
 
   def new
-    flash.keep
     self.resource = resource_class.new(sign_in_params)
     clean_up_passwords(resource)
     respond_with_navigational(resource, serialize_options(resource))
