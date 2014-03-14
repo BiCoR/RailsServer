03a066cb711f08ddb1c5244593a5ebb4ba9eef57
mfeustel <mike.feustel@it2011.ba-leipzig.de>
2014-03-14 19:26:05 +0100

    Assets neu kompiliert Ruby 1.9.3 eingestellt

--- push_heroku.cmd
+++ push_heroku.cmd
@@ -1,9 +1,9 @@
 git add .
 git commit -m "autocommit"
 call get_heroku_log.cmd
-call heroku run rake assets:clean
-call heroku run rake assets:clobber
+rem	call heroku run rake assets:clean
+rem	call heroku run rake assets:clobber
 git push origin master
 git push heroku master>push.log
-call heroku run rake assets:precompile
+rem	call heroku run rake assets:precompile
 pause
