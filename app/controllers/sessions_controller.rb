da33ad6607920f54bfd48c200d2bab6af8155001
mfeustel <mike.feustel@it2011.ba-leipzig.de>
2014-03-17 19:15:18 +0100

    autocommit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class SessionsController < Devise::SessionsController
 
+  # required for xml-output to apps
   def new
     self.resource = resource_class.new(sign_in_params)
     clean_up_passwords(resource)
